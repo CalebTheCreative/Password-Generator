964e9f4425947dc9d77fe8edb2c0935dd478babf
Caleb Hopkins <CalebHopkins@outlook.com>
2020-10-31 23:49:41 -0500

    Added answer possibilities.

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // Assignment Code
-// var generateBtn = document.querySelector("#generate");
+var generateBtn = document.querySelector("#generate");
 // var lowerCaseEl = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 // var upperCaseEl = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 // var numbersEl = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
@@ -9,12 +9,12 @@ var uppercaseEl = document.querySelector("askUpper");
 var lowercaseEl = document.querySelector("askLower");
 var numbersEl = document.querySelector("askUpper");
 var lowercaseEl = document.querySelector("askUpper");
-var criteria = {
-  "lowercase": "abcdefghijklmnopqrstuvwxyz",
-  "uppercase": "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
-  "numbers": "0123456789",
-  "special": "!#$%*=?@^"
-}
+// var criteria = {
+//   "lowercase": "abcdefghijklmnopqrstuvwxyz",
+//   "uppercase": "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
+//   "numbers": "0123456789",
+//   "special": "!#$%*=?@^"
+// }
 
 
 
@@ -25,48 +25,41 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
+// Alert
   alert("We need to ask a few questions regarding what criteria you'd like for your password.");
   var askCount = prompt("Choose the length of your password. (Character count must be 8-128 characters)");
-
+// Question about length
   if (askCount < 8 || askCount > 128) {
     alert("Please choose between 8 and 128 characters");
-    console.log("Character count not between 8-128.")
-    return askCount;
+    console.log("Character count not between 8-128.");
   }
-
-  else {
-    console.log("Good job");
-  }
-
+// Questions about criteria
   var askUpper = confirm("Would you like there to be uppercase letters? (A, B, C...)");
-
-  if (askUpper){
-    console.log("Uppercase");
-  }
-
   var askLower = confirm("Would you like there to be lowercase letters? (a, b, c...)");
-
-  if (askLower){
-    console.log("Lowercase");
-  }
-
   var askNumbers = confirm("Would you like there to be numbers? (1, 2, 3...)");
-
-  if (askNumbers){
-    console.log("Numbers");
-  }
-
   var askSpecial = confirm("Would you like there to be special characters? ($, !, ?...)");
 
-  if (askSpecial){
-    console.log("Special");
+  if (askUpper && askLower && askNumbers && askSpecial) {
+    console.log("All");
   }
 
+  if (askUpper === false && askLower && askNumbers && askSpecial) {
+    console.log("Lower, Numbers, Special");
+  }
+
+  if (askUpper === false && askLower === false && askNumbers && askSpecial) {
+    console.log("Numbers, Special");
+  }
+
+  if (askUpper === false && askLower && askNumbers && askSpecial) {
+    console.log("Lower, Numbers, Special");
+  }
+
+  
 }
 
 function generatePassword() {
-  console.log("generate password");
+  card-body.replace("Password");
 }
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
