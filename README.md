735b5d15c4ebddd53d51535fa868835e22670b56
Caleb Hopkins <CalebHopkins@outlook.com>
2020-10-31 23:24:17 -0500

    Updated README file

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
 # Password-Generator
+
+For this week's homework, we were asked to form a functioning password generator. We were given the HTML and CSS files, but needed to setup the JavaScript file to make it function.
+
+## Installation
+
+1) Create an array of each criteria (uppercase & lowercase letters, numbers, and special characters).
+
+2) Create a prompt asking the user to decide what how many characters they'd like their password to be when they press the "Generate Password" button.
+
+3) Proceed the prompt with questions confirming if the user would like to have uppercase, lowercase, numbers, special characters in their password.
+
+4) Using the answers from the previous questions, create a function to randomly generate a password within the length of characters the user set.
+
+5) Have random password displayed on screen.
+
+## Usage
+
+In order to use the application, press the button in the middle of the screen, titled "Generate Password". A series of questions will pop up for you to answer regarding your password length and criteria. As soon as you're finished answering, a password will appear on the screen.
+
+![password generator demo](Assets/Password-Generator.png)
+
+
+
--- script.js
+++ script.js
@@ -1,9 +1,23 @@
 // Assignment Code
-var generateBtn = document.querySelector("#generate");
-var lowerCaseEl = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-var upperCaseEl = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
-var numbersEl = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
-var specialEl = ["!", "#", "$", "%", "&", "*", "=", "?", "@", "^"];
+// var generateBtn = document.querySelector("#generate");
+// var lowerCaseEl = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+// var upperCaseEl = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+// var numbersEl = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+// var specialEl = ["!", "#", "$", "%", "&", "*", "=", "?", "@", "^"];
+var lengthEl = document.querySelector("askCount");
+var uppercaseEl = document.querySelector("askUpper");
+var lowercaseEl = document.querySelector("askLower");
+var numbersEl = document.querySelector("askUpper");
+var lowercaseEl = document.querySelector("askUpper");
+var criteria = {
+  "lowercase": "abcdefghijklmnopqrstuvwxyz",
+  "uppercase": "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
+  "numbers": "0123456789",
+  "special": "!#$%*=?@^"
+}
+
+
+
 
 // Write password to the #password input
 function writePassword() {
@@ -12,7 +26,7 @@ function writePassword() {
 
   passwordText.value = password;
 
-  alert("We need to ask a few questions regarding what parameters you'd like for your password.");
+  alert("We need to ask a few questions regarding what criteria you'd like for your password.");
   var askCount = prompt("Choose the length of your password. (Character count must be 8-128 characters)");
 
   if (askCount < 8 || askCount > 128) {
@@ -27,30 +41,27 @@ function writePassword() {
 
   var askUpper = confirm("Would you like there to be uppercase letters? (A, B, C...)");
 
-  if (askUpper === true){
+  if (askUpper){
     console.log("Uppercase");
   }
 
   var askLower = confirm("Would you like there to be lowercase letters? (a, b, c...)");
 
-  if (askLower === true){
+  if (askLower){
     console.log("Lowercase");
   }
 
   var askNumbers = confirm("Would you like there to be numbers? (1, 2, 3...)");
 
-  if (askNumbers === true){
+  if (askNumbers){
     console.log("Numbers");
   }
 
   var askSpecial = confirm("Would you like there to be special characters? ($, !, ?...)");
 
-  if (askSpecial === true){
+  if (askSpecial){
     console.log("Special");
   }
-
-
-
 
 }
 
